bb6784410fc1c17e295055b9d890315246c6af04
sevvalsimseklotusai <sevval.simsek@lotusaibilisim.com>
2024-05-17 15:38:44 +0300

    deploy files changed

--- deploy/update.sh
+++ deploy/update.sh
@@ -4,7 +4,6 @@ set -e
 
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
-cd $PROJECT_BASE_PATH
 git pull
 $PROJECT_BASE_PATH/env/bin/python manage.py migrate
 $PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
